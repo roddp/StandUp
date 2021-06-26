2e940cfb2b5dd239758972ed736e003c2d808cc1
Rodrigo <rodrigodomingospereira99@gmail.com>
2021-06-26 11:47:08 +0100

    a lot of changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,24 @@ import React from "react";
 import logo from "../assets/logo.png";
 
 const Home = () => {
-  return <img src={logo} alt="logo" />;
+  const style = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  const imgStyle = {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "50%",
+  };
+
+  return (
+    <div>
+      <h1 style={style}>StandUp</h1>
+      <img src={logo} alt="logo" style={imgStyle}></img>
+    </div>
+  );
 };
 
 export default Home;
